b644107215684ced12ddfd1f2c0b6c74f9ec201d
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-07-18 16:39:46 -0400

    commit tree 8f183a41c309ba7cc84375ba8e5fdb6a0df1d333

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,26 @@
+class PlantsController < ApplicationController
+    
+    def index
+        plant = Plant.all 
+        render json: plant
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        render json: plant
+    end
+
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant
+    end
+    
+
+    private
+    def plant_params
+        params.permit(:name,:image,:price)
+    end
+  
+end
+  
